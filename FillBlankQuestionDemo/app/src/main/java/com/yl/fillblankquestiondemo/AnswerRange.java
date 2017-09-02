91ba3dda38c43044ba428839c3761142b2e741e4
yangle <mr.yang0229@foxmail.com>
2017-09-02 14:04:48 +0800

    FillBlankQuestionDemo - 完善

---
+++ FillBlankQuestionDemo/app/src/main/java/com/yl/fillblankquestiondemo/AnswerRange.java
@@ -0,0 +1,17 @@
+package com.yl.fillblankquestiondemo;
+
+/**
+ * 答案范围
+ * Created by yangle on 2017/9/2.
+ */
+
+public class AnswerRange {
+
+    public int start;
+    public int end;
+
+    public AnswerRange(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+}
