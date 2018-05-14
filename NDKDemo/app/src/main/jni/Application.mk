75d2af65caf9953958dc9bd50420ac452666abb5
yangle <mr.yang0229@foxmail.com>
2018-05-14 15:09:22 +0800

    NDK r17 removes the armeabi, mips, and mips64 ABIs. https://github.com/android-ndk/ndk/issues/583

--- NDKDemo/app/src/main/jni/Application.mk
+++ NDKDemo/app/src/main/jni/Application.mk
@@ -2,4 +2,4 @@
 APP_MODULES := native-lib
 
 # 指定机器指令集
-APP_ABI := armeabi armeabi-v7a arm64-v8a x86 x86_64 mips mips64
+APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
--- NDKDemo/app/src/main/obj/local/mips64/objs/native-lib/native-lib.o.d
+++
@@ -1,4 +0,0 @@
-./obj/local/mips64/objs/native-lib/native-lib.o: jni/native-lib.cpp \
-  jni/com_yl_ndkdemo_MainActivity.h
-
-jni/com_yl_ndkdemo_MainActivity.h:
